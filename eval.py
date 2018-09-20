47d041888596081ebc4816acbc9cc100fae9beb7
Edvin Agnas <edvin.agnas@outlook.com>
2018-09-20 20:48:40 +0200

    evals 1

--- eval.py
+++ eval.py
@@ -20,6 +20,21 @@ import numpy as np
 
 # Confusion matrix for assessment step
 #from sklearn.metrics import confusion_matrix
+from keras.models import load_model
 
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+plt.imshow(X_test[2])
+
+print(X_train.shape)
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+
+X_train = X_train / 255
+X_test = X_test / 255
 
 model = load_model('my_model.h5')
+
+print(model.predict(X_test[2:3]))
+
+plt.show()
