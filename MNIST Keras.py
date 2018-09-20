d9548cbc9057a1988a513579034dfa8b6e6ac0e0
Edvin Agnas <edvin.agnas@outlook.com>
2018-09-20 19:38:11 +0200

    las keras

---
+++ MNIST Keras.py
@@ -0,0 +1,22 @@
+# The package for importing the dataset (already provided by Keras)
+from keras.datasets import mnist
+
+# Packages for defining the architecture of our model
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, BatchNormalization
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+
+# One-hot encoding
+from keras.utils import np_utils
+
+# Callbacks for training
+from keras.callbacks import TensorBoard, EarlyStopping
+
+# Ploting
+import matplotlib.pyplot as plt
+
+# Ndarray computations
+import numpy as np
+
+# Confusion matrix for assessment step
+from sklearn.metrics import confusion_matrix
