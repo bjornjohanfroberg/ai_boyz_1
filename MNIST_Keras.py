72744ee84a5f343a9a10ad59983929c173101dfe
Edvin Agnas <edvin.agnas@outlook.com>
2018-09-20 19:58:07 +0200

    ..

---
+++ MNIST_Keras.py
@@ -0,0 +1,59 @@
+# The package for importing the dataset (already provided by Keras)
+from keras.datasets import mnist
+
+# Packages for defining the architecture of our model
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, BatchNormalization
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+
+# One-hot encoding
+from keras.utils import np_utils
+
+# Callbacks for training
+from keras.callbacks import TensorBoard, EarlyStopping
+
+# Ploting
+import matplotlib.pyplot as plt
+
+# Ndarray computations
+import numpy as np
+
+# Confusion matrix for assessment step
+#from sklearn.metrics import confusion_matrix
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+plt.imshow(X_train[10], cmap='gray')
+
+print(X_train.shape)
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+print(X_train.shape)
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+
+def base_model():
+    # create model
+    model = Sequential()
+    model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# build the model
+model = base_model()
+
+# Fit the model
+tb = TensorBoard(log_dir='./logs/initial_setting')
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=64, callbacks=[tb])
+
